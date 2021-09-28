0c1b37766bdaa05be7ffb5f9c3e3ff56e6332266
pranshi-jindal <apranshi11@gmail.com>
2021-09-28 17:59:20 +0530

    Add files

---
+++ Assets/Scripts/DropdownHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownHandler : MonoBehaviour
+{
+    private string itemName;
+    public GameObject parent;
+    private Transform item;
+    public GameObject chef;
+
+
+    private void Start()
+    {
+        parent = GameObject.Find("Food Items");
+    }
+
+    public void HandleInput(int val)
+    {
+        var allFoodItems = GameObject.FindGameObjectsWithTag("Food Item");
+        foreach (var food in allFoodItems)
+            food.SetActive(false);
+
+        chef.SetActive(true);
+        itemName = gameObject.GetComponent<Dropdown>().options[val].text;
+        item = parent.transform.Find(itemName);
+        item.gameObject.SetActive(true);
+    }
+}
