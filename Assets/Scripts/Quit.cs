e37612d3ad2c0d050d19a087125f6e4919e961fc
pranshi-jindal <apranshi11@gmail.com>
2021-10-02 01:27:11 +0530

    Add background music and exit button

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
