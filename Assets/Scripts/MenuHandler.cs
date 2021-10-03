c15a305e1c325f22754e91008c28a9e878b108c0
Debashish Majumdar <majumdardebashish@ymail.com>
2021-10-03 11:32:28 +0530

    Create Menu Button

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour
+{
+
+    bool toggle = false;
+    public GameObject[] handler;
+    GameObject[] handler2;
+
+    //toggles SetActive of Game Objects
+    public void Handler()
+    {
+        if (toggle == false)
+        {
+            List<GameObject> goList = new List<GameObject>();
+            
+            for (int i = 0; i < handler.Length; i++)
+            {
+                if (handler[i].activeInHierarchy)
+                {
+                    handler[i].SetActive(false);
+                    goList.Add(handler[i]);
+                }
+            }
+            handler2 = goList.ToArray();
+            toggle = true;
+        }
+        else
+        {
+            for (int i = 0; i < handler2.Length; i++)
+            {
+                handler2[i].SetActive(true);
+            }
+            toggle = false;
+        }
+    }
+}
